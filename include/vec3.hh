3d044ac673afb08a66ad6fdbb40752bd36ae396c
emmanuelraob <108231611+emmanuelraob@users.noreply.github.com>
2023-06-20 15:00:50 -0600

    First simd operation
    First simd operation, dot
    the vec3 changed to use a vector of 4 numbers intead of 3

--- include/vec3.hh
+++ include/vec3.hh
@@ -4,12 +4,13 @@
 #include <cmath>
 #include <iostream>
 #include <rtweekend.hh>
+#include <immintrin.h>
 
 using std::sqrt;
 
 class vec3 {
     public:
-        vec3() : e{0,0,0} {}
+        vec3() : e{0,0,0,0} {}
         vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
         double x() const { return e[0]; }
@@ -43,7 +44,7 @@ class vec3 {
         }
 
         double length_squared() const {
-            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2] ;
         }
 
         inline static vec3 random() {
@@ -61,7 +62,7 @@ class vec3 {
         }
 
     public:
-        double e[3];
+        double e[4];
 };
 
 // Type aliases for vec3
@@ -99,9 +100,16 @@ inline vec3 operator/(vec3 v, double t) {
 }
 
 inline double dot(const vec3 &u, const vec3 &v) {
-    return u.e[0] * v.e[0]
-         + u.e[1] * v.e[1]
-         + u.e[2] * v.e[2];
+
+    __m256d vec1 = _mm256_loadu_pd(u.e);
+    __m256d vec2 = _mm256_loadu_pd(v.e);
+
+    __m256d result = _mm256_mul_pd(vec1, vec2);
+    
+    double res[4];
+    _mm256_storeu_pd(res, result);
+
+    return res[0] + res[1] + res[2];
 }
 
 inline vec3 cross(const vec3 &u, const vec3 &v) {
