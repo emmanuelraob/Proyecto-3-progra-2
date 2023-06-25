a92ea02b8eb9bf1a249323171b1680c4ea0fb67e
hsin.chang <hsin.chang@ucr.ac.cr>
2023-06-25 15:07:56 -0600

    Bigger Sphere and Smaller Sphere

--- include/hittable_list.hh
+++ include/hittable_list.hh
@@ -2,10 +2,11 @@
 #define HITTABLE_LIST_HH
 
 #include <hittable.hh>
-
+#include <sphere.hh>
 #include <memory>
 #include <vector>
 
+using std::vector;
 using std::shared_ptr;
 using std::make_shared;
 
@@ -18,6 +19,10 @@ class hittable_list : public hittable {
         void add(shared_ptr<hittable> object);
 
         virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+        virtual hittable* biggerSphere(vector<hittable*> esferas, int dimension) = 0;
+
+        virtual hittable* smallerSphere(vector<hittable*> esferas, int dimension) = 0;
 
     public:
         std::vector<shared_ptr<hittable>> objects;
